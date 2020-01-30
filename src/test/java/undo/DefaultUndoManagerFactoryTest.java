eea38e403880e63eee4bd413437cf9e9f78a10a7
Aleksandrs Gusevs <aleksandrs.gusevs@proofit.lv>
2020-01-30 19:19:43 +0200

    Created initial solution structure with not implemented unit tests

---
+++ src/test/java/undo/DefaultUndoManagerFactoryTest.java
@@ -0,0 +1,14 @@
+package undo;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class DefaultUndoManagerFactoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
