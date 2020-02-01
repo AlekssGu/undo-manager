d2ee806c4365bee1b70d6b95501d7d5d714b420a
Aleksandrs Gusevs <aleksandrs.gusevs@proofit.lv>
2020-02-01 08:54:41 +0200

    Implemented UndoManager interface

--- src/main/java/undo/DefaultUndoManager.java
+++ src/main/java/undo/DefaultUndoManager.java
@@ -1,40 +1,98 @@
 package undo;
 
+import java.util.Queue;
+
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+
 import change.Change;
 import document.Document;
 
 class DefaultUndoManager implements UndoManager {
 
 	private Document document;
-	private int bufferSize;
+	private Queue<Change> currentChanges;
+	private Queue<Change> changesToRedo;
 
 	DefaultUndoManager(Document document, int bufferSize) {
 		this.document = document;
-		this.bufferSize = bufferSize;
+		this.currentChanges = new CircularFifoQueue<>(bufferSize);
+		this.changesToRedo = new CircularFifoQueue<>(bufferSize);
 	}
 
 	@Override
 	public void registerChange(Change change) {
-
+		this.currentChanges.add(change);
 	}
 
 	@Override
 	public boolean canUndo() {
-		return false;
+		return hasChangesToUndo();
 	}
 
 	@Override
 	public void undo() {
-
+		if (this.canUndo()) {
+			Change change = getLatestChangeToRevert();
+			revertChange(change);
+			putRevertedChangeToRedoRegister(change);
+		} else {
+			throw new IllegalStateException();
+		}
 	}
 
 	@Override
 	public boolean canRedo() {
-		return false;
+		return hasChangesToRedo();
 	}
 
 	@Override
 	public void redo() {
+		if (this.canRedo()) {
+			Change change = getLatestChangeToRedo();
+			applyChange(change);
+			putAppliedChangeToCurrentChangeRegister(change);
+		} else {
+			throw new IllegalStateException();
+		}
+	}
 
+	private void revertChange(Change changeToUndo) {
+		try {
+			changeToUndo.revert(this.document);
+		} catch (Exception exception) {
+			throw new IllegalStateException(exception);
+		}
+	}
+
+	private void putRevertedChangeToRedoRegister(Change change) {
+		this.changesToRedo.add(change);
+	}
+
+	private void putAppliedChangeToCurrentChangeRegister(Change change) {
+		this.currentChanges.add(change);
+	}
+
+	private boolean hasChangesToUndo() {
+		return !this.currentChanges.isEmpty();
+	}
+
+	private boolean hasChangesToRedo() {
+		return !this.changesToRedo.isEmpty();
+	}
+
+	private Change getLatestChangeToRevert() {
+		return this.currentChanges.poll();
+	}
+
+	private Change getLatestChangeToRedo() {
+		return this.changesToRedo.poll();
+	}
+
+	private void applyChange(Change changeToRedo) {
+		try {
+			changeToRedo.apply(this.document);
+		} catch (Exception exception) {
+			throw new IllegalStateException(exception);
+		}
 	}
 }
--- src/test/java/undo/DefaultUndoManagerTest.java
+++ src/test/java/undo/DefaultUndoManagerTest.java
@@ -1,14 +1,81 @@
 package undo;
 
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.willThrow;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import change.Change;
+import document.Document;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DefaultUndoManagerTest {
 
+	private static final int BUFFER_SIZE_VALUE = 1;
+
+	@Mock
+	private Document document;
+	@Mock
+	private Change change;
+
+	@InjectMocks
+	private DefaultUndoManager undoManager = new DefaultUndoManager(document, BUFFER_SIZE_VALUE);
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void registersChangesMadeToDocumentInUndoRegister() {
+		undoManager.registerChange(change);
+		assertThat(undoManager.canUndo()).isTrue();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void throwsExceptionIfUndoInvokedOnEmptyUndoRegister() {
+		undoManager.undo();
+	}
+
+	@Test
+	public void revertsLatestChangeAndPutsItIntoRedoRegister() {
+		undoManager.registerChange(change);
+
+		undoManager.undo();
+
+		assertThat(undoManager.canRedo()).isTrue();
+	}
+
+	@Test
+	public void appliesLatestUndoChangeAndPutsItIntoChangeRegister() {
+		undoManager.registerChange(change);
+
+		undoManager.undo();
+		undoManager.redo();
+
+		assertThat(undoManager.canRedo()).isFalse();
+		assertThat(undoManager.canUndo()).isTrue();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void throwsExceptionWhenRevertChangeFailed() {
+		willThrow(RuntimeException.class).given(change).revert(document);
+		undoManager.registerChange(change);
+
+		undoManager.undo();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void throwsExceptionWhenApplyChangeFailed() {
+		willThrow(RuntimeException.class).given(change).apply(document);
+		undoManager.registerChange(change);
+		undoManager.undo();
+
+		undoManager.redo();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void throwsExceptionIfRedoInvokedOnEmptyRedoRegister() {
+		undoManager.redo();
 	}
 
 }
